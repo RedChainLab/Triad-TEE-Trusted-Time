f6493018ba848536d1dd41371d139f8e0e79c82b
Jean <jean.edouardacker@gmail.com>
2024-08-13 19:50:16 +0800

    try to fix blocking calibration

--- node/server/enc/server_class.cpp
+++ node/server/enc/server_class.cpp
@@ -187,6 +187,12 @@ void Server::handle_client(node_connection &client){
             runtime_scheduler->canSend = NOT_DELAYED;
             //t_print("SERVER : canSend : %d\n", runtime_scheduler->canSend);
         }
+        else if (res == TIMESTAMP){
+            t_print("SERVER : ts received from other node : %lld\n", runtime_scheduler->timestamps);     
+        }
+        else if(res == PORT){
+            t_print("SERVER : received request from %d nodes\n", res);
+        }
         runtime_scheduler->epoch = epoch;
         runtime_scheduler->epoch -= runtime_scheduler->timestamps;
         sgx_thread_mutex_unlock(&runtime_scheduler->mutex);
