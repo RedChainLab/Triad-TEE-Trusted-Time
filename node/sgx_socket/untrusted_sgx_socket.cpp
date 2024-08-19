0da2ddfa1fa7e3c48db1ec59c9f9db41c8b8287d
Jean <jean.edouardacker@gmail.com>
2024-08-19 20:11:24 +0800

    ajout des defs de recvfrom et sendto

--- node/sgx_socket/untrusted_sgx_socket.cpp
+++ node/sgx_socket/untrusted_sgx_socket.cpp
@@ -87,6 +87,30 @@ ssize_t u_recv(int sockfd, void *buf, size_t nbytes, int flags)
     return recv(sockfd, buf, nbytes, flags);
 }
 
+ssize_t u_sendto(
+            int sockfd, 
+            const void *buf, 
+            size_t len, 
+            int flags,
+            const struct sockaddr *dest_addr,
+            socklen_t addrlen
+            )
+{
+    return sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+}
+
+ssize_t u_recvfrom(
+            int sockfd, 
+            void *buf, 
+            size_t len, 
+            int flags,
+            struct sockaddr *src_addr,
+            socklen_t *addrlen
+            )
+{
+    return recvfrom(sockfd, buf, len, flags, src_addr, addrlen);
+}
+
 int u_setsockopt(
 			int sockfd, 
 			int level, 
