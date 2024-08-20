ccf5f35bbf8ce4e13b08c58d5a0093007a13e9eb
Jean <jean.edouardacker@gmail.com>
2024-08-20 19:10:31 +0800

    tentative de fix de recvfrom

--- node/sgx_socket/trusted_sgx_socket.cpp
+++ node/sgx_socket/trusted_sgx_socket.cpp
@@ -136,7 +136,7 @@ ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct 
     return -1;
 }
 
-ssize_t recvfrom(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen)
+ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *dest_addr, socklen_t* addrlen)
 {
     ssize_t ret = 0;
 
