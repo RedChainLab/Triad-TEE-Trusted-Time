467ed6d1602e7de87c3e986e0978356f95914e08
mbettinger <49953678+mbettinger@users.noreply.github.com>
2024-09-30 13:56:02 +0000

    Triad workflow in main

--- triad_udp/user/main.cpp
+++ triad_udp/user/main.cpp
@@ -11,12 +11,13 @@ int main(int argc, char* argv[]) {
     uint16_t port = atoi(argv[1]);
     int core_rdTSC = atoi(argv[2]);
     Node* node = Node::get_instance(port, core_rdTSC);
-    Node::get_instance(port+1, core_rdTSC+1);
     Node* node2 = Node::get_instance(port+1, core_rdTSC+1);
+    Node* node3 = Node::get_instance(port+2, core_rdTSC+2);
     node->get_timestamp();
 
     usleep(10000);
     node->add_sibling("127.0.0.1", port+1);
+    node->add_sibling("127.0.0.1", port+2);
 
     std::cout << "<Enter anything to continue>"<< std::endl;
     std::string msg;
@@ -25,13 +26,14 @@ int main(int argc, char* argv[]) {
     for (int i = 3; i < argc; i += 2) {
         node->add_sibling(argv[i], atoi(argv[i+1]));
         node2->add_sibling(argv[i], atoi(argv[i+1]));
+        node3->add_sibling(argv[i], atoi(argv[i+1]));
     }
 
     std::cout << "<Enter anything to continue>"<< std::endl;
     std::cin >> msg;
 
     Node::destroy_instance(port);
-    Node::destroy_instance(port);
     Node::destroy_instance(port+1);
+    Node::destroy_instance(port+2);
     return 0;
 }
