35993e42578d505ba7e13e09cefc06fcd325a1b8
mbettinger <49953678+mbettinger@users.noreply.github.com>
2025-03-26 16:01:11 +0000

    Enhanced addcount figure

--- AEXTest/analysis/plot_count_distributions.py
+++ AEXTest/analysis/plot_count_distributions.py
@@ -48,29 +48,33 @@ for timestamp in timestamps:
       except Exception as e:
         print(f'Error reading {f}: {e}\n')
     agg_df.columns = ['count']
-    fig, ax = plt.subplots()
-    ax.hist(agg_df['count'], bins=200, label='AEX', histtype='step', linewidth=1)
+    fig, ax = plt.subplots(figsize=(4.5,2.5))
+    # ax.hist(agg_df['count'], bins=200, label='AEX', histtype='step', linewidth=1)
     ax.hist(agg_df['count'], bins=200, label='AEX', histtype='step', linewidth=1, cumulative=True)
     ax.set_xlabel('Number of increments')
     ax.set_ylabel('Number of runs')
 
     y_closest_power_of_10 = 10 ** (math.floor(math.log10(agg_df['count'].count()))-1)
-    ax.set_yticks(np.arange(0, agg_df['count'].count()+y_closest_power_of_10/2, y_closest_power_of_10))
+    if not log:
+      ax.set_yticks(np.arange(0, agg_df['count'].count()+y_closest_power_of_10/2, y_closest_power_of_10))
 
     x_closest_power_of_10 = 10 ** math.floor(math.log10(agg_df['count'].max()))
     #ax.set_xticks(np.arange(0, agg_df['count'].max()+x_closest_power_of_10/2, x_closest_power_of_10/2))
     #ax.set_xticks(np.arange(0, agg_df['count'].max()+x_closest_power_of_10/2, x_closest_power_of_10/10), minor=True)
 
     #ax.set_xlim(agg_df['count'].min()-x_closest_power_of_10/2, agg_df['count'].max()+x_closest_power_of_10/2)
+    ax.set_ylim(0)
     if log:
       ax.set_yscale('log')
+      ax.set_ylim(0.5)
     ax.grid(True, which='major', linestyle='-')
-    ax.grid(True, which='minor', linestyle=':', alpha=0.8)
+    ax.grid(True, axis="y", which='minor', linestyle='-', alpha=0.5)
+    ax.minorticks_on()
 
-    rel_error=(agg_df['count'].max()-agg_df['count'].min())/agg_df['count'].mean()
-    ax.legend(labels=["$\\frac{\\Delta x}{\\bar{x}}="+f'{rel_error:2E}$'], loc='center right')
+    # rel_error=(agg_df['count'].max()-agg_df['count'].min())/agg_df['count'].mean()
+    # ax.legend(labels=["$\\frac{\\Delta x}{\\bar{x}}="+f'{rel_error:2E}$'], loc='center right')
 
     print("mean:",agg_df['count'].mean(),"std:",agg_df['count'].std())
 
-    fig.savefig(f'fig/count-{timestamp}-{sgx_type}-{sleep_type}-{sleep_time_secs}{"-log" if log else ""}.png', bbox_inches='tight', dpi=1200)
+    fig.savefig(f'fig/count-{timestamp}-{sgx_type}-{sleep_type}-{sleep_time_secs}{"-log" if log else ""}.pdf', bbox_inches='tight', dpi=1200)
 
